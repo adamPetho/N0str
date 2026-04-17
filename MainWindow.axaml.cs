621331f16ab82619db143f4e78c09fa2d421d2eb
adamPetho <petho.adam@hotmail.com>
2026-04-17 17:27:36 +0200

    Add project files.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace N0str
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
