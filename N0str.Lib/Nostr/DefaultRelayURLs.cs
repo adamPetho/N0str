7fa63e3dc93592d450d8e9bae5c809b5e08cffeb
adamPetho <petho.adam@hotmail.com>
2026-04-20 19:22:27 +0200

    Add default relay urls

---
+++ N0str.Lib/Nostr/DefaultRelayURLs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N0str.Nostr
+{
+    public static class DefaultRelayURLs
+    {
+        public static readonly string[] URLs = 
+            ["wss://relay.primal.net",
+            "wss://nos.lol",
+            "wss://relay.damus.io"];
+    }
+}
