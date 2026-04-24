b2bdc47e5b35914e77e6b55ff83f1d12028a219d
adamPetho <petho.adam@hotmail.com>
2026-04-24 20:08:40 +0200

    add views

---
+++ N0str.Lib/Views/Pages/CreateEvent.axaml.cs
@@ -0,0 +1,16 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace N0str.Views.Pages;
+
+public partial class CreateEvent : UserControl
+{
+    public CreateEvent()
+    {
+        if (!Avalonia.Threading.Dispatcher.UIThread.CheckAccess())
+            throw new Exception("CreateEvent constructor called off UI thread!");
+
+        InitializeComponent();
+    }
+}
---
+++ N0str.Lib/Views/Pages/FetchPostsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace N0str.Views.Pages;
+
+public partial class FetchPostsView : UserControl
+{
+    public FetchPostsView()
+    {
+        InitializeComponent();
+    }
+}
