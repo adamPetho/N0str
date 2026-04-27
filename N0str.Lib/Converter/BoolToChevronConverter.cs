beddaacb2c3440e6cc062569c0e43baa26f9ed66
adamPetho <petho.adam@hotmail.com>
2026-04-27 15:07:37 +0200

    Create BoolToChevron converter

---
+++ N0str.Lib/Converter/BoolToChevronConverter.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N0str.Converter
+{
+    public class BoolToChevronConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value is true ? "▾" : "▸";
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
