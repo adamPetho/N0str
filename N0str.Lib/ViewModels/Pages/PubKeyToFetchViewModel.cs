ba15ebc1e6deb6660c9bc19c4ecbd4093cb17cde
adamPetho <petho.adam@hotmail.com>
2026-05-05 21:27:07 +0200

    add view and VM to collect pubkey to fetch

---
+++ N0str.Lib/ViewModels/Pages/PubKeyToFetchViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using N0str.Nostr;
+using N0str.Services;
+using NBitcoin;
+using NNostr.Client;
+using ReactiveUI;
+using System.Collections.ObjectModel;
+using System.Reactive;
+
+namespace N0str.ViewModels.Pages
+{
+    public class PubKeyToFetchViewModel : ViewModelBase
+    {
+        private string _pubKey = string.Empty;
+        private string? _errorMessage;
+        private readonly INavigation _navigationService;
+        private readonly IServiceProvider _serviceProvider;
+
+        public PubKeyToFetchViewModel(INavigation navigationService, IServiceProvider serviceProvider)
+        {
+            _navigationService = navigationService;
+            _serviceProvider = serviceProvider;
+
+            BackCommand = ReactiveCommand.Create(() => _navigationService.CloseModal());
+
+            var canConfirm = this.WhenAnyValue(
+                x => x.PublicKey,
+                key => !string.IsNullOrWhiteSpace(key));
+
+            FetchEventsCommand = ReactiveCommand.CreateFromTask(FetchEventsFromPubKey, canConfirm);
+        }
+
+        public string PublicKey
+        {
+            get => _pubKey;
+            set
+            {
+                SetProperty(ref _pubKey, value);
+            }
+        }
+
+        //TODO: Check if string is actually npub or not and display error
+        public string? ErrorMessage
+        {
+            get => _errorMessage;
+            set
+            {
+                SetProperty(ref _errorMessage, value);
+                OnPropertyChanged(nameof(HasError));
+            }
+        }
+
+        public bool HasError => !string.IsNullOrEmpty(_errorMessage);
+
+        public ReactiveCommand<Unit, Unit> BackCommand { get; }
+        public ReactiveCommand<Unit, Unit> FetchEventsCommand { get; }
+
+        private async Task FetchEventsFromPubKey()
+        {
+            var requestedPubKey = PublicKey;
+
+            //_navigationService.NavigateTo();
+        }
+
+    }
+}
