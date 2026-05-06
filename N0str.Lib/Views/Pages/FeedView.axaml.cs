1ecd64658f96eeb9c4fd8152982f728be9d3c9e3
adamPetho <petho.adam@hotmail.com>
2026-05-06 20:59:17 +0200

    add FeedView (empty for now)

---
+++ N0str.Lib/Views/Pages/FeedView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace N0str.Views.Pages;
+
+public partial class FeedView : UserControl
+{
+    public FeedView()
+    {
+        InitializeComponent();
+    }
+}
