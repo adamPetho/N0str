df2a2178a4a8f11641d642bf6cba511742d5083e
adamPetho <petho.adam@hotmail.com>
2026-05-07 19:50:49 +0200

    LoadingVM

---
+++ N0str.Lib/ViewModels/Pages/LoadingViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N0str.ViewModels.Pages
+{
+    public class LoadingViewModel : ViewModelBase
+    {
+        private string _statusMessage = string.Empty;
+
+        public string StatusMessage
+        {
+            get => _statusMessage;
+            set => SetProperty(ref _statusMessage, value);
+        }
+    }
+}
