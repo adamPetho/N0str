9d426e2b2e6c10d348eeba591b8a690e2fa0e8d6
adamPetho <petho.adam@hotmail.com>
2026-05-08 17:49:16 +0200

    add Tests project and the first unit test

---
+++ N0str.Tests/UnitTests/ConverterTests.cs
@@ -0,0 +1,26 @@
+﻿using N0str.Converter;
+using System.Globalization;
+
+namespace N0str.Tests.UnitTests
+{
+    public class ConverterTests
+    {
+        [Fact]
+        public void BoolToChevronConverter_True_Test()
+        {
+            var value = true;
+            var converter = new BoolToChevronConverter();
+
+            Assert.Equal("▾", converter.Convert(value, typeof(bool), parameter: null, CultureInfo.InvariantCulture));
+        }
+
+        [Fact]
+        public void BoolToChevronConverter_False_Test()
+        {
+            var value = false;
+            var converter = new BoolToChevronConverter();
+
+            Assert.Equal("▸", converter.Convert(value, typeof(bool), parameter: null, CultureInfo.InvariantCulture));
+        }
+    }
+}
