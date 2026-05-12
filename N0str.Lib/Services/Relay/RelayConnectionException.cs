4b4b7a8596cfccedb0dbc045f38f9c5c4b76094e
adamPetho <petho.adam@hotmail.com>
2026-05-12 15:41:14 +0200

    add RelayConnectionException

---
+++ N0str.Lib/Services/Relay/RelayConnectionException.cs
@@ -0,0 +1,18 @@
+﻿namespace N0str.Services.Relay
+{
+    [Serializable]
+    internal class RelayConnectionException : Exception
+    {
+        public RelayConnectionException()
+        {
+        }
+
+        public RelayConnectionException(string? message) : base(message)
+        {
+        }
+
+        public RelayConnectionException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
