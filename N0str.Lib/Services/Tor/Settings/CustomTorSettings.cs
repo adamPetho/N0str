85303f09af34a98423c5cb8a5ea005404395096b
adamPetho <petho.adam@hotmail.com>
2026-05-20 21:04:38 +0200

    Separate TorSettings from TorService

---
+++ N0str.Lib/Services/Tor/Settings/CustomTorSettings.cs
@@ -0,0 +1,19 @@
+﻿using OnionSharp.Microservices;
+using OnionSharp.Tor;
+using System.Net;
+
+namespace N0str.Services.Tor.Settings
+{
+    public class CustomTorSettings : ITorSettings
+    {
+        public TorSettings _torSettings = new(
+            EnvironmentHelpers.GetDataDir("N0str"),
+            EnvironmentHelpers.GetFullBaseDirectory(),
+            terminateOnExit: true,
+            socksPort: 37155,
+            controlPort: 37156);
+
+        public TorSettings GetTorSettings() => _torSettings;
+        public EndPoint GetSocksEndpoint() => _torSettings.SocksEndpoint;
+    }
+}
---
+++ N0str.Lib/Services/Tor/Settings/ITorSettings.cs
@@ -0,0 +1,11 @@
+﻿using OnionSharp.Tor;
+using System.Net;
+
+namespace N0str.Services.Tor.Settings
+{
+    public interface ITorSettings
+    {
+        TorSettings GetTorSettings();
+        EndPoint GetSocksEndpoint();
+    }
+}
