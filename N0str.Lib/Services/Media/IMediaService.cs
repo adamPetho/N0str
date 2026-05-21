c4bf1dd09e0cac468e96324dde4fd907fdf88762
adamPetho <petho.adam@hotmail.com>
2026-05-21 20:01:40 +0200

    add MediaService to download image bytes

---
+++ N0str.Lib/Services/Media/IMediaService.cs
@@ -0,0 +1,7 @@
+﻿namespace N0str.Services.Media
+{
+    public interface IMediaService
+    {
+        Task<byte[]> FetchImageBytesFromUrl(string url);
+    }
+}
---
+++ N0str.Lib/Services/Media/MediaService.cs
@@ -0,0 +1,24 @@
+﻿using N0str.Services.Tor.Settings;
+using OnionSharp.Microservices;
+using OnionSharp.Tor;
+
+namespace N0str.Services.Media
+{
+    public class MediaService : IMediaService
+    {
+        private readonly ITorSettings _torSettings;
+        private readonly OnionHttpClientFactory _torHttpClientFactory;
+
+        public MediaService(ITorSettings torSettings)
+        {
+            _torSettings = torSettings;
+            _torHttpClientFactory = new OnionHttpClientFactory(_torSettings.GetSocksEndpoint().ToUri("socks5"));
+        }
+
+        public async Task<byte[]> FetchImageBytesFromUrl(string url)
+        {
+            using var onionHttpClient = _torHttpClientFactory.CreateClient($"image:{url}");
+            return await onionHttpClient.GetByteArrayAsync(url);
+        }
+    }
+}
