79b751530dbede2de78e2b8142bdc42763a9e920
adamPetho <petho.adam@hotmail.com>
2026-05-21 20:02:12 +0200

    add test

---
+++ N0str.Tests/UnitTests/MediaServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using N0str.Services.Media;
+using N0str.Services.Tor.Settings;
+using OnionSharp.Microservices;
+using OnionSharp.Tor;
+
+namespace N0str.Tests.UnitTests
+{
+    public class MediaServiceTests
+    {
+        [Theory]
+        [InlineData("https://r2.primal.net/cache/8/74/2f/8742f1977fcc732a8297f119a0beb3f78171b899cc35b4d378dcc1dcaed1a832.jpg")]
+        public async Task Fetch_IMG_URL_Test(string imageURL)
+        {
+            var customTorSettings = new CustomTorSettings();
+
+            TorProcessManager torProcessManager = new TorProcessManager(customTorSettings.GetTorSettings());
+            await torProcessManager.StartAsync(attempts: 3, CancellationToken.None);
+
+            var mediaService = new MediaService(customTorSettings);
+
+            var result = await mediaService.FetchImageBytesFromUrl(imageURL);
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+
+            await torProcessManager.DisposeAsync();
+        }
+    }
+}
