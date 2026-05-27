5f0be8c186e62cc973149547c2d048e7d0c6328b
adamPetho <petho.adam@hotmail.com>
2026-05-27 20:02:31 +0200

    add placeholder Channel class

---
+++ N0str.Lib/Services/Media/MediaPipelineChannel.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+
+namespace N0str.Services.Media
+{
+    public class MediaPipelineChannel
+    {
+        public Channel<string> MediaChannel { get; }
+        public MediaPipelineChannel()
+        {
+            MediaChannel = Channel.CreateUnbounded<string>(new UnboundedChannelOptions
+            {
+                SingleReader = true,
+                SingleWriter = false
+            });
+        }
+
+        public async Task EnqueueLink(string url)
+        {
+            await MediaChannel.Writer.WriteAsync(url);
+        }
+
+        public async IAsyncEnumerable<string> ReadAllAsync([EnumeratorCancellation]CancellationToken ct)
+        {
+            await foreach (var request in MediaChannel.Reader.ReadAllAsync(ct)) 
+            {
+                yield return request;
+            }
+        }
+    }
+}
