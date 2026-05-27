304377fa2d53d6e7a40bd62d683e8b67b7e8f95c
adamPetho <petho.adam@hotmail.com>
2026-05-27 20:02:56 +0200

    WIP mediaPipelineService

---
+++ N0str.Lib/Services/Media/IMediaPipelineService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N0str.Services.Media
+{
+    public interface IMediaPipelineService
+    {
+        Task EnqueueURL(string url);
+
+        Task ReadChannelLoop(CancellationToken ct);
+    }
+}
---
+++ N0str.Lib/Services/Media/MediaPipelineService.cs
@@ -0,0 +1,42 @@
+﻿namespace N0str.Services.Media
+{
+    public class MediaPipelineService : IMediaPipelineService
+    {
+        private readonly MediaPipelineChannel _mediaChannel;
+        private readonly IMediaService _mediaFetcherService;
+        private readonly CancellationTokenSource _cts = new();
+
+        private readonly Task _readerThread;
+
+        public MediaPipelineService(MediaPipelineChannel mediaChannel, IMediaService mediaService)
+        {
+            _mediaChannel = mediaChannel;
+            _mediaFetcherService = mediaService;
+
+            _readerThread = Task.Run(async () => ReadChannelLoop(_cts.Token));
+        }
+
+        public async Task EnqueueURL(string url)
+        {
+            await _mediaChannel.EnqueueLink(url);
+        }
+
+        public async Task ReadChannelLoop(CancellationToken ct)
+        {
+            await foreach (var request in _mediaChannel.ReadAllAsync(ct))
+            {
+                try
+                {
+                    var imageBytes = await _mediaFetcherService.FetchImageBytesFromUrl(request);
+                    // decode bitmap
+                    // update VM, raise Event?
+                }
+                catch (Exception ex)
+                {
+                    //TODO: Better Logging
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+    }
+}
