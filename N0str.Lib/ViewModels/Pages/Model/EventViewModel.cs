d78325b0cd98d7f03fdcc359d8ad3b3808def5ad
adamPetho <petho.adam@hotmail.com>
2026-05-29 20:05:47 +0200

    Add dedicated Event VM and View

---
+++ N0str.Lib/ViewModels/Pages/Model/EventViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia.Media.Imaging;
+using NNostr.Client;
+using ReactiveUI;
+using System.Collections.ObjectModel;
+using System.Reactive;
+
+
+namespace N0str.ViewModels.Pages.Model
+{
+    public class EventViewModel : ViewModelBase
+    {
+        public string ID { get; }
+        public string? Content { get; }
+
+        public string AuthorPubKey { get; }
+
+        public DateTimeOffset? CreatedAt { get; }
+
+        public int Kind { get; }
+        public List<NostrEventTag> Tags { get; }
+
+        public ObservableCollection<Bitmap> Images { get; } = [];
+
+        public bool IsMediaLoading { get; set; }
+
+        public bool HasMedia => Images.Count > 0;
+
+        public EventViewModel(NostrEvent ev)
+        {
+            ID = ev.Id;
+            Content = ev.Content;
+            AuthorPubKey = ev.PublicKey;
+            CreatedAt = ev.CreatedAt;
+            Kind = ev.Kind;
+            Tags = ev.Tags;
+        }
+    }
+}
