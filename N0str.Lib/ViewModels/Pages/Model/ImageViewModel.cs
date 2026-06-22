9a7629b3719fe75a18b3ce2d68812f6c851b80fb
adamPetho <petho.adam@hotmail.com>
2026-06-22 19:44:50 +0200

    add ImageView and VM

---
+++ N0str.Lib/ViewModels/Pages/Model/ImageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Media.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N0str.ViewModels.Pages.Model
+{
+    public class ImageViewModel : ViewModelBase
+    {
+        public string Url { get; }
+        public Bitmap Bitmap { get; }
+        public int Width => Bitmap.PixelSize.Width;
+        public int Height => Bitmap.PixelSize.Height;
+
+        public ImageViewModel(string url, Bitmap bitmap)
+        {
+            Url = url;
+            Bitmap = bitmap;
+        }
+    }
+}
