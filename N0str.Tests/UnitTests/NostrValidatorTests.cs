96a20d507c7bb942e6fc471bf6c8d5cc5f835689
adamPetho <petho.adam@hotmail.com>
2026-06-23 21:41:59 +0200

    Add test

--- N0str.Tests/UnitTests/NostrValidatorTests.cs
+++ N0str.Tests/UnitTests/NostrValidatorTests.cs
@@ -18,5 +18,18 @@ namespace N0str.Tests.UnitTests
 
             Assert.True(result);
         }
+
+        [Theory]
+        [InlineData("npub1jw7scmeuewhywwytqxkxec9jcqf3znw2fsyddcn3948lw9q950ps9y35fg")]
+        [InlineData("nprofile1qqsf80gvdu7vhtj88z9srtrvuzevqyc3fh9yczxkufcj6nlhzsz68scmupga8")]
+        public void CanGetSamePubKeyFromNpubAndNprofile(string value)
+        {
+            var expectedPubkey = "93bd0c6f3ccbae47388b01ac6ce0b2c013114dca4c08d6e2712d4ff71405a3c3";
+            var res = Checkers.TryExtractIdentifier(value, out string? hex);
+
+            Assert.True(res);
+            Assert.NotNull(hex);
+            Assert.Equal(expectedPubkey, hex);
+        }
     }
 }
