f520baad3875ee05926ee2f358a684ee3c9b28fa
adamPetho <petho.adam@hotmail.com>
2026-06-23 21:41:30 +0200

    fix relayservice subscription

--- N0str.Lib/Services/Relay/RelayService.cs
+++ N0str.Lib/Services/Relay/RelayService.cs
@@ -80,7 +80,7 @@ namespace N0str.Services.Relay
 
         public async Task CreateSubscriptionAsync(string pubkey, string subscriptionID, CancellationToken ct = default)
         {
-            string pubKeyHex = NIP19.FromNIP19Npub(pubkey).ToHex();
+            string pubKeyHex = pubkey;
             await NostrClient.CreateSubscription(subscriptionID, [new() { Kinds = [1], Authors = [pubKeyHex] }], ct);
 
         }
