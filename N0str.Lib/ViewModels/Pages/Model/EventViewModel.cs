c181363a476b123c73aa37d293d519c1daf8038b
adamPetho <petho.adam@hotmail.com>
2026-07-02 18:10:24 +0200

    Build EventVM with Root and Reply referenced events

--- N0str.Lib/ViewModels/Pages/Model/EventViewModel.cs
+++ N0str.Lib/ViewModels/Pages/Model/EventViewModel.cs
@@ -1,53 +1,71 @@
 ﻿using Avalonia.Media.Imaging;
 using Avalonia.Threading;
+using N0str.Models;
 using N0str.Static;
 using NNostr.Client;
-using ReactiveUI;
 using System.Collections.ObjectModel;
-using System.Reactive;
-using System.Runtime.CompilerServices;
 
 
 namespace N0str.ViewModels.Pages.Model
 {
     public class EventViewModel : ViewModelBase
     {
-        public string ID { get; }
-        public string? Content { get; }
+        public NostrEvent NostrEvent { get; }
+        public List<EventReference> References { get; }
+
+        // Third party root event replied to
+        public NostrEvent? RootEvent { get; }
+        // The Event the Pubkey replied to
+        public NostrEvent? ReplyEvent { get; }
         public string? DisplayContent { get; }
-
-        public string AuthorPubKey { get; }
-
-        public DateTimeOffset? CreatedAt { get; }
-
-        public int Kind { get; }
-        public List<NostrEventTag> Tags { get; }
 
         public ObservableCollection<ImageViewModel> Images { get; } = [];
 
         public bool IsMediaLoading { get; set; }
 
         public bool HasMedia => Images.Count > 0;
+        public EventType EventType { get; }
 
-        public EventViewModel(NostrEvent ev)
+        public EventViewModel(NostrEventWithReferences ev)
         {
-            ID = ev.Id;
-            Content = ev.Content;
-            AuthorPubKey = ev.PublicKey;
-            CreatedAt = ev.CreatedAt;
-            Kind = ev.Kind;
-            Tags = ev.Tags;
-
-            DisplayContent = Content;
+            NostrEvent = ev.NostrEvent;
+            References = ev.References;
+            RootEvent = ExtractRootEventIfExists(ev);
+            ReplyEvent = ExtractReplyEventIfExists(ev);
+            EventType = SetEventType();
+            DisplayContent = NostrEvent.Content;
 
             if (DisplayContent is null)
                 return;
 
-            var imageURLs = MediaExtractor.ExtractImageUrls(ev);
+            var imageURLs = MediaExtractor.ExtractImageUrls(ev.NostrEvent);
             foreach (var imageURL in imageURLs)
             {
                 DisplayContent = DisplayContent.Replace(imageURL, "");
             }
+        }
+
+        private EventType SetEventType()
+        {
+            return RootEvent is not null ? EventType.Reply : EventType.Root;
+        }
+
+        private NostrEvent? ExtractRootEventIfExists(NostrEventWithReferences ev)
+        {
+            if (ev.References.Count == 0) 
+                return null;
+            
+            var rootEventID = ev.References.Where(ev => ev.Type == EventType.Root).Select(ev => ev.EventId).FirstOrDefault();
+            return ev.ReferencedEvents.Where(ev => ev.Id == rootEventID).FirstOrDefault();
+        }
+
+        private NostrEvent? ExtractReplyEventIfExists(NostrEventWithReferences ev)
+        {
+            if (ev.References.Count == 0)
+                return null;
+
+            var replyEventID = ev.References.Where(ev => ev.Type == EventType.Reply).Select(ev => ev.EventId).FirstOrDefault();
+            return ev.ReferencedEvents.Where(ev => ev.Id == replyEventID).FirstOrDefault();
         }
 
         public async Task AddImageToEvent(string imgURL, byte[] imageBytes)
