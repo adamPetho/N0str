6d2bd534658bb8d67bd65c72f2dd310ca94f3598
adamPetho <petho.adam@hotmail.com>
2026-07-02 19:21:01 +0200

    Add EventReference View

---
+++ N0str.Lib/Views/Pages/Model/EventReferenceView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace N0str.Views.Pages.Model;
+
+public partial class EventReferenceView : UserControl
+{
+    public EventReferenceView()
+    {
+        InitializeComponent();
+    }
+}
