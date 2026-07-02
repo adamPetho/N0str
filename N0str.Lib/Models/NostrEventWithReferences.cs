0ad376629f6fb9ad8817fe776a9178ea2f775d4a
adamPetho <petho.adam@hotmail.com>
2026-07-02 18:08:34 +0200

    Add NostrEventWithReferences placeholder model

---
+++ N0str.Lib/Models/NostrEventWithReferences.cs
@@ -0,0 +1,24 @@
+﻿using NNostr.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N0str.Models
+{
+    public class NostrEventWithReferences
+    {
+        public NostrEvent NostrEvent { get; }
+        public List<EventReference> References { get; } = [];
+        public List<NostrEvent> ReferencedEvents { get;  } = [];
+
+        public NostrEventWithReferences(NostrEvent nostrEvent, List<EventReference> references, List<NostrEvent> referencedEvents)
+        {
+            NostrEvent = nostrEvent;
+            References = references;
+            ReferencedEvents = referencedEvents;
+        }
+
+    }
+}
