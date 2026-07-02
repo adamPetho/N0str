5c15e503afd621d69f630cee1072f6ca739ca750
adamPetho <petho.adam@hotmail.com>
2026-07-02 18:07:59 +0200

    Add EventReference model

---
+++ N0str.Lib/Models/EventReference.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N0str.Models
+{
+    public record EventReference
+    {
+        public required string EventId { get; init; }
+
+        public string? RelayUrl { get; init; }
+
+        public EventType Type { get; init; }
+    }
+
+    public enum EventType
+    {
+        Root,
+        Reply,
+        Mention,
+        Unknown
+    }
+}
