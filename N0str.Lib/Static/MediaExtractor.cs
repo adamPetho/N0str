02eed82e1572efd200d0846f19ab8590a414f483
adamPetho <petho.adam@hotmail.com>
2026-08-04 18:40:28 +0200

    useful helper method for image Url

--- N0str.Lib/Static/MediaExtractor.cs
+++ N0str.Lib/Static/MediaExtractor.cs
@@ -19,6 +19,18 @@ namespace N0str.Static
                 .ToList();
         }
 
+        public static IReadOnlyList<string> ExtractImageUrls(string eventText)
+        {
+            if (string.IsNullOrWhiteSpace(eventText))
+                return [];
+
+            return ImageUrlRegex
+                .Matches(eventText)
+                .Select(x => x.Value)
+                .Distinct(StringComparer.OrdinalIgnoreCase)
+                .ToList();
+        }
+
         public static bool TryGetImageUrls(NostrEvent nostrEvent, out IReadOnlyList<string> links)
         {
             links = ExtractImageUrls(nostrEvent);
@@ -28,6 +40,22 @@ namespace N0str.Static
             }
 
             return false;
+        }
+
+        public static bool TryGetImageUrls(string eventText, out IReadOnlyList<string> links)
+        {
+            links = ExtractImageUrls(eventText);
+            if (links.Count > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsImageUrl(string url)
+        {
+            return ImageUrlRegex.IsMatch(url);
         }
     }
 }
