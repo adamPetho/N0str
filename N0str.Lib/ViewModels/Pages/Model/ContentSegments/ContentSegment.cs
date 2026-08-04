208677eadb80713483303fdcce153453caeaf881
adamPetho <petho.adam@hotmail.com>
2026-08-04 18:39:48 +0200

    ContentSegments

---
+++ N0str.Lib/ViewModels/Pages/Model/ContentSegments/ContentSegment.cs
@@ -0,0 +1,28 @@
+﻿namespace N0str.ViewModels.Pages.Model.ContentSegments
+{
+    public abstract class ContentSegment
+    {
+    }
+
+    public sealed class TextSegment : ContentSegment
+    {
+        public string Text { get; init; }
+    }
+
+    public sealed class MentionSegment : ContentSegment
+    {
+        public string NProfile { get; init; }
+
+        public string DisplayName { get; set; }
+    }
+
+    public sealed class ImageSegment : ContentSegment
+    {
+        public string Url { get; init; }
+    }
+
+    public sealed class LinkSegment : ContentSegment
+    {
+        public string Url { get; init; }
+    }
+}
