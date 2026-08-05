7d3ae7b17b1f4374712ad55e7ddc915ee6146778
adamPetho <petho.adam@hotmail.com>
2026-08-05 15:41:05 +0200

    put Image related methods and props in ImageVM

--- N0str.Lib/ViewModels/Pages/Model/ImageViewModel.cs
+++ N0str.Lib/ViewModels/Pages/Model/ImageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Media.Imaging;
+using Avalonia.Threading;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +11,36 @@ namespace N0str.ViewModels.Pages.Model
     public class ImageViewModel : ViewModelBase
     {
         public string Url { get; }
-        public Bitmap Bitmap { get; }
-        public int Width => Bitmap.PixelSize.Width;
-        public int Height => Bitmap.PixelSize.Height;
+        private Bitmap? _bitmap;
+        public Bitmap? Bitmap
+        {
+            get => _bitmap;
+            set => SetProperty(ref _bitmap, value);
+        }
+        public int? Width => Bitmap?.PixelSize.Width;
+        public int? Height => Bitmap?.PixelSize.Height;
+        public bool HasImage => Bitmap != null;
 
-        public ImageViewModel(string url, Bitmap bitmap)
+        public ImageViewModel(string url, Bitmap? bitmap)
         {
             Url = url;
             Bitmap = bitmap;
+        }
+
+        public async Task AddImageToEvent(byte[] imageBytes)
+        {
+            var bitmap = ConvertBytesToBitmap(imageBytes);
+
+            await Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                Bitmap = bitmap;
+            });
+        }
+
+        private Bitmap ConvertBytesToBitmap(byte[] bytes)
+        {
+            using var ms = new MemoryStream(bytes);
+            return new Bitmap(ms);
         }
     }
 }
