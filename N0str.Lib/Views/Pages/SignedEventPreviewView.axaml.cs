142cd3fbe6973ee281942c902808ed234082c4cb
adamPetho <petho.adam@hotmail.com>
2026-08-25 19:26:22 +0200

    add SignedEventPreview View and VM

---
+++ N0str.Lib/Views/Pages/SignedEventPreviewView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace N0str.Views.Pages;
+
+public partial class SignedEventPreviewView : UserControl
+{
+    public SignedEventPreviewView()
+    {
+        InitializeComponent();
+    }
+}
