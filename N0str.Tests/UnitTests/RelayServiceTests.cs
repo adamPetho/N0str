88a2e5f572fd9dabbefaaa4920f614f9632ec335
adamPetho <petho.adam@hotmail.com>
2026-08-28 19:04:14 +0200

    fix tests

--- N0str.Tests/UnitTests/RelayServiceTests.cs
+++ N0str.Tests/UnitTests/RelayServiceTests.cs
@@ -4,7 +4,6 @@ using N0str.Services.Relay;
 using N0str.Services.Tor;
 using N0str.Services.Tor.Settings;
 using NNostr.Client;
-using OnionSharp.Tor;
 using System.Net;
 using System.Net.WebSockets;
 
@@ -26,75 +25,65 @@ namespace N0str.Tests.UnitTests
         }
 
         [Fact]
-        public async Task Connection_Retry_Mechanism_Test()
-        {
-            // Mock the Nostr Client and it's connection to the relays.
-            var mockedNostrClient = new Mock<INostrClient>();
-            mockedNostrClient.SetupSequence(x => x.ConnectAndWaitUntilConnected(It.IsAny<CancellationToken>()))
-                .ThrowsAsync(new WebSocketException())
-                .ThrowsAsync(new WebSocketException())
-                .Returns(Task.CompletedTask);
-
-            // Mock successful event publishing
-            mockedNostrClient
-                .Setup(x => x.PublishEvent(
-                    It.IsAny<NostrEvent>(),
-                    It.IsAny<CancellationToken>()))
-                .Returns(Task.CompletedTask);
-
-            _nostrClientFactory.Setup(x => x.Create(It.IsAny<Uri[]>(), It.IsAny<EndPoint?>())).Returns(mockedNostrClient.Object);
-
-            bool eventForwarded = false;
-
-            _relayService.EventReceived += _ =>
-            {
-                eventForwarded = true;
-            };
-
-
-            await _relayService.ConnectAsync(["wss://relay.primal.net", "wss://nos.lol", "wss://relay.damus.io"]);
-
-            mockedNostrClient.Raise(
-                x => x.EventsReceived += null,
-                mockedNostrClient.Object,
-                ("sub-1", new[]
-                {
-                    new NostrEvent
-                    {
-                        Id = "event-1"
-                    }
-                }));
-
-            mockedNostrClient.Verify(
-                x => x.ConnectAndWaitUntilConnected(It.IsAny<CancellationToken>()),
-                Times.Exactly(3));
-
-            Assert.True(eventForwarded);
-        }
-
-        [Fact]
         public async Task Throw_If_All_Retries_Falls()
         {
             // Mock the Nostr Client to fail 3 times.
             var mockedNostrClient = new Mock<INostrClient>();
             mockedNostrClient.SetupSequence(x => x.ConnectAndWaitUntilConnected(It.IsAny<CancellationToken>()))
-                .ThrowsAsync(new WebSocketException())
-                .ThrowsAsync(new WebSocketException())
-                .ThrowsAsync(new WebSocketException());
-
-            mockedNostrClient
-                .Setup(x => x.PublishEvent(
-                    It.IsAny<NostrEvent>(),
-                    It.IsAny<CancellationToken>()))
-                .Returns(Task.CompletedTask);
+                .ThrowsAsync(new RelayConnectionException())
+                .ThrowsAsync(new RelayConnectionException())
+                .ThrowsAsync(new RelayConnectionException())
+                .ThrowsAsync(new RelayConnectionException()); //throw on clearnet as well
 
             _nostrClientFactory.Setup(x => x.Create(It.IsAny<Uri[]>(), It.IsAny<EndPoint?>())).Returns(mockedNostrClient.Object);
 
             await Assert.ThrowsAsync<RelayConnectionException>(() => _relayService.ConnectAsync(["wss://relay.primal.net", "wss://nos.lol", "wss://relay.damus.io"]));
 
+            // 4 times = 3 onion connection + 1 fallback clearnet connection failed.
             mockedNostrClient.Verify(
                 x => x.ConnectAndWaitUntilConnected(It.IsAny<CancellationToken>()),
-                Times.Exactly(3));
+                Times.Exactly(4));
+        }
+
+        [Fact]
+        public async Task FallBack_To_Clearnet()
+        {
+            var mockedNostrClient = new Mock<INostrClient>();
+            mockedNostrClient.SetupSequence(x => x.ConnectAndWaitUntilConnected(It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new RelayConnectionException())
+                .ThrowsAsync(new RelayConnectionException())
+                .ThrowsAsync(new RelayConnectionException());
+
+            var mockedFallbackNostrClient = new Mock<INostrClient>();
+            mockedFallbackNostrClient.Setup(x => x.ConnectAndWaitUntilConnected(It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+
+            // 3 relay setup (matching the test case)
+            _nostrClientFactory
+                .Setup(x => x.Create(
+                    It.Is<Uri[]>(uris =>
+                        uris.SequenceEqual(new[]
+                        {
+                            new Uri("wss://relay.primal.net"),
+                            new Uri("wss://nos.lol"),
+                            new Uri("wss://relay.damus.io")
+                        })),
+                    It.IsAny<EndPoint?>()))
+                .Returns(mockedNostrClient.Object);
+
+            // Specific 1-relay case, called by RelayService on clearnet fallback
+            _nostrClientFactory
+                .Setup(x => x.Create(
+                    It.Is<Uri[]>(uris => uris.SequenceEqual(new[]
+                    {
+                        new Uri("wss://relay.primal.net")
+                    })),
+                    It.IsAny<EndPoint?>()))
+                .Returns(mockedFallbackNostrClient.Object);
+
+
+            // After 3 failure, we fall back to clearnet.
+            await _relayService.ConnectAsync(["wss://relay.primal.net", "wss://nos.lol", "wss://relay.damus.io"]);
         }
     }
 }
